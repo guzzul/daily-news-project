0f8f02d54c90b8b1cb85ff4c8b60deb90e23272a
Andrew Magnaye <milbert.andrew.magnaye@codeandtheory.com>
2026-04-23 06:51:46 +0800

    chore: update search skeleton card to 6

--- components/search-loading.tsx
+++ components/search-loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SearchLoading() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {[...Array(3)].map((_, i) => (
+      {[...Array(6)].map((_, i) => (
         <div key={i} className="space-y-4">
           <Skeleton className="aspect-[16/10] w-full rounded-none" />
           <Skeleton className="h-6 w-3/4 rounded-none" />
